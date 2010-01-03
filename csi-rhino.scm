33b1ef5c409089053d3e85882ab9402171a915c1
Thomas Bushnell BSG <tb@kozaki.becket.net>
2010-01-02 21:20:41 -0800

    compile library too

--- csi-rhino.scm
+++ csi-rhino.scm
@@ -21,19 +21,38 @@
 	(newline js-connector)
 	(flush-output js-connector))
 
+      (define (compile-and-run-1 prepared k)
+	(let ((transformed (cps-transform prepared k)))
+	  (let-values (((g-set!s l-set!s) 
+			(find-modifications (list transformed))))
+	    (let ((compiled (compile-form (simplify transformed
+						    '() l-set!s))))
+	      (send-out (string-append compiled ";"))))))
+
+      (define (compile-and-run form k)
+	(let-values ((forms (prepare form)))
+	  (cond
+	   ((= (length forms) 1) (compile-and-run-1 (car forms) k))
+	   ((> (length forms) 1)
+	    (compile-and-run-1 `(begin ,@forms) k)))))
+
       (send-out "load('runtime.js')\n")
       (send-out "load('rhino.js')\n")
       (send-out "rhino_initialize();")
+      (call-with-input-file "library.scm"
+	(lambda (lib)
+	  (let next ((expr (read lib)))
+	    (if (not (eof-object? expr))
+		(begin
+		  ;(display expr) (newline)
+		  (compile-and-run expr 'sinjs_repl_noprint_k)
+		  (next (read lib)))))))
       (let repl ()
 	(display "sinjs> ")
 	(flush-output)
 	(let ((expr (read)))
 	  (if (not (eof-object? expr))
-	      (let ((transformed  (cps-transform (prepare expr) 'sinjs_repl_k)))
-		(let-values (((g-set!s l-set!s) 
-			      (find-modifications (list transformed))))
-		  (let ((compiled (compile-form (simplify transformed
-							  #f l-set!s))))
-		    (send-out (string-append compiled ";"))
-		    (sleep 1)
-		    (repl))))))))))
+	      (begin
+		(compile-and-run expr 'sinjs_repl_k)
+		(sleep 1)
+		(repl))))))))
